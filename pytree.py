1b40be06d13fe78a1edc0e2f10183f34584af94d
ylynn <wy249@cornell.edu>
2016-10-02 04:35:55 +0000

    tree

--- pytree.py
+++ pytree.py
@@ -3,34 +3,35 @@
 import sys
 import os
 
-def print_tree(path, count, indent=''):
-  items = os.listdir(path)
-  for i, files in enumerate(items):
-    fullpath = path + "/" + files
-    if i == len(items) - 1:
-      print(indent + '└── ' + files)
-      count[1] = count[1] + 1
-      if os.path.isdir(fullpath):
-        print_tree(fullpath, count, indent+'   ')
-        count[0] = count[0] + 1
-    else:
-      print(indent + '├── ' + files)
-      count[1] = count[1] + 1
-      if os.path.isdir(fullpath):
-        print_tree(fullpath, count, indent+'│   ')
-        count[0] = count[0] + 1
 
-if __name__ == '__main__' :
-  if (len(sys.argv) == 1):
-    dirname = "."
-    count = [0, 0]
-  elif (len(sys.argv) == 2):
-    dirname= sys.argv[1]
-    count = [0, 0]
-  else:
-    print('Wrong Input')
-    sys.exit()
-  print(dirname)
-  print_tree(dirname, count)
-  print()
-  print('%d directories, %d files' % (count[0], count[1]-count[0]))
+def print_tree(path, count, indent=''):
+    items = os.listdir(path)
+    items = [item for item in items if item[0] != '.']
+    for i, files in enumerate(items):
+        fullpath = path + "/" + files
+        count[1] = count[1] + 1
+        if i == len(items) - 1:
+            print(indent + '`-- ' + files)
+            if os.path.isdir(fullpath):
+                print_tree(fullpath, count, indent + '   ')
+                count[0] = count[0] + 1
+        else:
+            print(indent + '|-- ' + files)
+            if os.path.isdir(fullpath):
+                print_tree(fullpath, count, indent + '|   ')
+                count[0] = count[0] + 1
+
+if __name__ == '__main__':
+    if (len(sys.argv) == 1):
+        dirname = "."
+        count = [0, 0]
+    elif (len(sys.argv) == 2):
+        dirname = sys.argv[1]
+        count = [0, 0]
+    else:
+        print('Wrong Input')
+        sys.exit()
+    print(dirname)
+    print_tree(dirname, count)
+    print()
+    print('%d directories, %d files' % (count[0], count[1] - count[0]))
