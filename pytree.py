5454cb5a6dd856b2700121872c8d73f86494c9ca
ylynn <wy249@cornell.edu>
2016-10-02 03:32:53 +0000

    tree

--- pytree.py
+++ pytree.py
@@ -3,18 +3,22 @@
 import sys
 import os
 
-def print_tree(path, count, indent=""):
+def print_tree(path, count, indent=''):
   items = os.listdir(path)
   for i, files in enumerate(items):
     fullpath = path + "/" + files
     if i == len(items) - 1:
-      print (indent + '`-- ' + files)
+      print(indent + '`-- ' + files)
+      count[1] = count[1] + 1
+      if os.path.isdir(fullpath):
+        print_tree(fullpath, count, indent+'   ')
+        count[0] = count[0] + 1
     else:
-      print (indent + '|-- ' + files)
-    count[1] = count[1] + 1
-    if os.path.isdir(fullpath):
-      print_tree(fullpath, count, indent+'|   ')
-      count[0] = count[0] + 1
+      print(indent + '|-- ' + files)
+      count[1] = count[1] + 1
+      if os.path.isdir(fullpath):
+        print_tree(fullpath, count, indent+'|   ')
+        count[0] = count[0] + 1
 
 if __name__ == '__main__' :
   if (len(sys.argv) == 1):
@@ -24,9 +28,9 @@ if __name__ == '__main__' :
     dirname= sys.argv[1]
     count = [0, 0]
   else:
-    print ('Wrong Input')
+    print('Wrong Input')
     sys.exit()
-  print (dirname)
-  print_tree (dirname, count)
-  print ()
-  print ('%d directories, %d files' % (count[0], count[1]-count[0]))
+  print(dirname)
+  print_tree(dirname, count)
+  print()
+  print('%d directories, %d files' % (count[0], count[1]-count[0]))
