21e573d33497cc8e155fd66ab2ad32136ca3a22b
ylynn <wy249@cornell.edu>
2016-10-02 03:05:25 +0000

    tree assignment

--- pytree.py
+++ pytree.py
@@ -1,11 +1,32 @@
 #!/usr/bin/env python3
-import subprocess
+
 import sys
+import os
 
+def print_tree(path, count, indent=""):
+  items = os.listdir(path)
+  for i, files in enumerate(items):
+    fullpath = path + "/" + files
+    if i == len(items) - 1:
+      print (indent + '`-- ' + files)
+    else:
+      print (indent + '|-- ' + files)
+    count[1] = count[1] + 1
+    if os.path.isdir(fullpath):
+      print_tree(fullpath, count, indent+'|   ')
+      count[0] = count[0] + 1
 
-# YOUR CODE GOES here
-
-
-if __name__ == '__main__':
-    # just for demo
-    subprocess.run(['tree'] + sys.argv[1:])
+if __name__ == '__main__' :
+  if (len(sys.argv) == 1):
+    dirname = "."
+    count = [0, 0]
+  elif (len(sys.argv) == 2):
+    dirname= sys.argv[1]
+    count = [0, 0]
+  else:
+    print ('Wrong Input')
+    sys.exit()
+  print (dirname)
+  print_tree (dirname, count)
+  print ()
+  print ('%d directories, %d files' % (count[0], count[1]-count[0]))
